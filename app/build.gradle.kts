9342b19866e4a578827fb9a462f014cf8c974b78
abhimehtaa339 <abhimehtaa339@gmail.com>
2023-09-02 23:33:49 +0530

    animated loading bar dependency or maven jitpack to setting_gradel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation("com.github.ybq:Android-SpinKit:1.4.0")
     implementation("com.android.volley:volley:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
